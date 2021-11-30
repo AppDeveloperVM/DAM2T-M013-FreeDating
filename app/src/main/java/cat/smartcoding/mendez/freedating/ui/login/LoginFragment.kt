a154fa6a39502ff027dec066c04aac0a5996718b
msabate00 <sabatemiranda@gmail.com>
2021-11-30 16:35:17 +0100

    MSM - Logica login inicial

--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/login/LoginFragment.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/login/LoginFragment.kt
@@ -1,18 +1,21 @@
 package cat.smartcoding.mendez.freedating.ui.login
 
-import android.app.Fragment
+import android.content.ContentValues.TAG
+import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import cat.smartcoding.mendez.freedating.R
 import cat.smartcoding.mendez.freedating.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
-class LoginFragment : FragmentActivity() {
+class LoginFragment : Fragment() {
     private lateinit var viewModel: LoginViewModel
     private lateinit var binding: FragmentLoginBinding
 
@@ -29,9 +32,36 @@ class LoginFragment : FragmentActivity() {
         )
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java];
         binding.viewModel = viewModel;
-        //binding.lifecycleOwner = viewLife
+        binding.lifecycleOwner = viewLifecycleOwner;
 
 
         return binding.root;
     }
+
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        val auth = viewModel.getAuth();
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                //btAutentifica.isEnabled = true
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                   // updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                   // Toast.makeText(baseContext, "Authentication failed.",
+                    //    Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+
+
+
 }
--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/login/LoginViewModel.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/login/LoginViewModel.kt
@@ -1,12 +1,15 @@
 package cat.smartcoding.mendez.freedating.ui.login
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginViewModel : ViewModel() {
 
     private lateinit var auth: FirebaseAuth;
+
 
 
     public fun onCreateView(){
@@ -15,14 +18,30 @@ class LoginViewModel : ViewModel() {
 
     fun onStart(){
 
+        val currentUser = auth.currentUser
+
+        if(currentUser != null){
+            reload();
+        }
+    }
+    private fun reload() {
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+    private fun updateUI(user: FirebaseUser?) {
+        if( user != null ) {
+
+        }else{
+
+        }
     }
 
 
-
-    override fun onCleared() {
-        super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+    fun getAuth(): FirebaseAuth{
+        return auth;
     }
+
+
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
