ecfb99debd72a741928b6c5c1578a41187a5de46
msabate00 <sabatemiranda@gmail.com>
2021-12-15 20:40:26 +0100

    MSM - Inicio User

--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/login/RegisterFragment.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/login/RegisterFragment.kt
@@ -87,6 +87,7 @@ class RegisterFragment : Fragment(), AdapterView.OnItemSelectedListener {
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
+        //a
 
     }
 
