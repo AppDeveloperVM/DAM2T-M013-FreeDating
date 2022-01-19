4d05244e474250882aed5a80e5bf2503db81b56b
msabate00 <71668554+msabate00@users.noreply.github.com>
2022-01-19 20:03:13 +0100

    MSM (desde casa) - Night mode

--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/settings/SettingsFragment.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/settings/SettingsFragment.kt
@@ -1,15 +1,48 @@
 package cat.smartcoding.mendez.freedating.ui.settings
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
 import cat.smartcoding.mendez.freedating.MainActivity
 import cat.smartcoding.mendez.freedating.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+    override fun onResume() {
+        Log.i("HELP",preferenceManager.sharedPreferencesName);
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        super.onPause()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        when(key){
+
+            "night_mode" -> {
+                when (sharedPreferences?.getBoolean(key, false)){
+                    true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+            "gendeer" ->{
+
+            }
+            "age" ->{
+
+            }
+
+        }
     }
 
 }
