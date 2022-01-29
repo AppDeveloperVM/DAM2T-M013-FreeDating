1872e4aeead74a01fcee0e97bc64445f61decd56
Víctor Maree Redondo <victor.maree.redondo@copernic.alumnat.cat>
2022-01-29 11:35:12 +0100

    Maree Vic - ProfileItem updated with more params

--- app/src/main/java/cat/smartcoding/mendez/freedating/ui/profiles/ProfileItem.kt
+++ app/src/main/java/cat/smartcoding/mendez/freedating/ui/profiles/ProfileItem.kt
@@ -1,5 +1,54 @@
 package cat.smartcoding.mendez.freedating.ui.profiles
 
 import android.graphics.Bitmap
+import android.os.Parcel
+import android.os.Parcelable
 
-data class ProfileItem(var image: Bitmap? = null, var name : String? = "", var gender : String? = "", var birthdate : String? = "")
+//@Parcelize
+data class ProfileItem(
+    var image: Bitmap? = null,
+    var name : String? = "",
+    var gender : String? = "",
+    var birthdate : String? = "",
+    var email: String? = "",
+    var location: String? = "",
+    var otherThings : String? = "",
+    var description : String?= "",
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(Bitmap::class.java.classLoader),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(image, flags)
+        parcel.writeString(name)
+        parcel.writeString(gender)
+        parcel.writeString(birthdate)
+        parcel.writeString(email)
+        parcel.writeString(location)
+        parcel.writeString(otherThings)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ProfileItem> {
+        override fun createFromParcel(parcel: Parcel): ProfileItem {
+            return ProfileItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ProfileItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
